efef4c0c312f799df273462c4c7bfaff8e423629
Prakash Acharya <pracharya2601@gmail.com>
2022-02-14 09:10:48 -0800

    remove svgr webpack

--- next.config.js
+++ next.config.js
@@ -25,11 +25,6 @@ module.exports = withBundleAnalyzer({
     },
     webpack: (config) => {
         // extend your webpack configuration here
-        config.module.rules.push({
-            test: /\.svg$/i,
-            issuer: /\.[jt]sx?$/,
-            use: ['@svgr/webpack'],
-        });
         return config;
     },
 });
